92f72f258765a9262ed8a053f4e71983567c249d
MFlanders <mflanders06@gmail.com>
2021-02-03 21:11:22 -0700

    Finished higher-order up to reduce

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,13 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
+
+
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers  = mixedNumbers.filter( (val, i, arr) => {
+  return val % 2 === 0;
+}
+)
 
 
 
@@ -44,7 +49,10 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices  = prices.map( (val, i, arr) => {
+  return val * 1.07;
+}
+);
 
 
 
@@ -63,7 +71,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation  //=  populations.reduce( /* Provide Your Callback Here */ );
 
 
 
