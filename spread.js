052b60df1dc6c2c183392d4133fc37477ec26794
MFlanders <mflanders06@gmail.com>
2021-02-08 20:08:29 -0700

    finished spread

--- spread.js
+++ spread.js
@@ -30,7 +30,7 @@ console.log(productCopy);
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
-
+let colorsCopy = [...colors];
 
 
 
@@ -45,6 +45,7 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
+result = addNums(...numbers);
 
 
 ////////// PROBLEM 4 //////////
@@ -55,3 +56,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+let combinedArrays = [...arr1, ...arr2];
